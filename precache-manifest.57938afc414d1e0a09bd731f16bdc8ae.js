b10bcc7e0adb95c48e4d09d4ecda24681fc1b93a
meikuan23 <meikuan23@yahoo.com>
2022-05-02 15:11:29 +0800

    Updates

---
+++ precache-manifest.57938afc414d1e0a09bd731f16bdc8ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "de4122c2f67aee6ed0e9",
+    "url": "/robofriend/static/css/main.e20e567d.chunk.css"
+  },
+  {
+    "revision": "de4122c2f67aee6ed0e9",
+    "url": "/robofriend/static/js/main.de4122c2.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/robofriend/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "98d89c36e12d892f91ea",
+    "url": "/robofriend/static/css/2.bd64cdfc.chunk.css"
+  },
+  {
+    "revision": "98d89c36e12d892f91ea",
+    "url": "/robofriend/static/js/2.98d89c36.chunk.js"
+  },
+  {
+    "revision": "0c913e01bdcad5cda96e",
+    "url": "/robofriend/static/js/runtime~main.0c913e01.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriend/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "2bcbdc1e76bd63055a7529b0acce5c28",
+    "url": "/robofriend/index.html"
+  }
+];
